9c6d4fdd8fb39e297d58c448c13b29dbe0810505
Charlotte <cfe224@lehigh.edu>
2021-03-09 23:51:24 -0500

    Assignment 03A.R

---
+++ Assignment 03A.R
@@ -0,0 +1,7 @@
+# Assignment 03A.R
+# Create an R script using a FOR loop to print the squares of the integers 
+# from 25 through 1 (in that order) in the console, with each value being 
+# printed on a new line.
+for (i in 25:1) 
+{cat(25:1,sep="\n")}
+
